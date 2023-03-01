6bef1511c019623a2d3d38d237643a12b87ba16e
unknown <kevintran351996@gmail.com>
2023-03-01 11:18:00 +0700

    cloudVps get api

--- src/services/apis/index.ts
+++ src/services/apis/index.ts
@@ -463,3 +463,14 @@ export const getSubOrderPagesByParent = (slug: string) =>
 
 export const getProductsBySubOrderPage = (id: string) =>
     axiosInstanceNew.get(`${preUrl}/products/products-by-sub-order-page/${id}`)
+
+
+    export const getCloudVpsByUserIdVietTell= (
+        // userId: string,
+        // areaId: string,
+        // operatingSystemId: string,
+        search: string,
+        pageIndex: number,
+        pageSize: number
+    ) =>
+    axiosInstanceNew.get(`${preUrl}/listVMS/getpaging?search=${search}&pageIndex=${pageIndex}&pageSize=${pageSize}`)
